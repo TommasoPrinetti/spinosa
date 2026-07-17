743393d2d284a0ef7a7869a922460029eeeb983e
TommasoPrinetti <Tommi.prinetti@gmail.com>
2026-07-17 15:57:39 +0200

    ci: use spinosa typecheck for beta release

--- script/verify-release.sh
+++ script/verify-release.sh
@@ -6,6 +6,6 @@ bash -n workspace-template/.bin/spinosa
 test -f workspace-template/.spinosa/workspace-files.tsv
 test -f workspace-template/.bin/spinosa
 
-bun run --cwd packages/tui typecheck
+bun run --cwd packages/tui typecheck:spinosa
 bun run --cwd packages/tui test:spinosa
 bun run script/build-tui.ts --single --skip-install
